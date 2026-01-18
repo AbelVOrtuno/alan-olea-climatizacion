d9493f15e846d7a30fac5db290856aeaf9499952
Abel <152920117+AbelVOrtuno@users.noreply.github.com.>
2026-01-18 14:53:24 -0600

    "estructura completa de nuestro proyecto con sus dependencias de desarrollo"

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+
+
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+// Compilar SASS
+function css() {
+  return src('src/sass/app.scss')
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(sass({ quietDeps: true }))
+    .pipe(dest('dist/css'));
+} 
+
+// Vigilar cambios
+function dev() {
+  watch('src/sass/**/*.scss', css);
+}
+
+exports.css = css;
+exports.dev = dev;
